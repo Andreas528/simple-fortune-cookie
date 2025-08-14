a2b76907de348937cb5265ef7cabe36e69563e39
Mads Jeppesen <majep23@student.sdu.dk>
2025-08-14 10:57:59 +0000

    Changed which Docker repository gets pushed to (#4)

--- ci/push-docker.sh
+++ ci/push-docker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 echo "$docker_password" | docker login ghcr.io --username "$docker_username" --password-stdin
-docker push "ghcr.io/$docker_username/frontend:1.0-${GIT_COMMIT::8}" 
-docker push "ghcr.io/$docker_username/frontend:latest"
-docker push "ghcr.io/$docker_username/backend:1.0-${GIT_COMMIT::8}" 
-docker push "ghcr.io/$docker_username/backend:latest" &
+docker push "ghcr.io/$docker_username/simple-fortune-cookie:1.0-${GIT_COMMIT::8}" 
+docker push "ghcr.io/$docker_username/simple-fortune-cookie:latest"
+docker push "ghcr.io/$docker_username/simple-fortune-cookie:1.0-${GIT_COMMIT::8}" 
+docker push "ghcr.io/$docker_username/simple-fortune-cookie:latest" &
 wait
